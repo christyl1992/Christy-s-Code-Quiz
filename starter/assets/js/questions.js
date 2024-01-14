264696f5080cfe3ee226685fa7066d9dc5f5ed65
Christy Llewellyn <christy.elizabeth@hotmail.co.uk>
2024-01-14 18:58:01 +0000

    Added five quiz questions to the questions page.

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,32 +1,27 @@
-
 var questions = [
-	{
-		question: "Who is the sexiest coding teacher EVER?",
-		answers: {
-            choices: ["Moose", "Alexandre", "Gazpacho"],
-			// a: 'Moose',
-			// b: 'Alexandre',
-			// c: 'Gazpacho'
-		},
-	}
-];
-
-
-let currentQuestionIndex = 0;
-
-
-function loadQuestion() {
-    const currentQuestion = questions[0];
-// Update question title
-    document.getElementById('question-title').textContent = currentQuestion.question;
-// Update choices 
-  const choicesContainer = document.getElementById('choices');
-  choicesContainer.innerHTML = '';
-// Create and append choice buttons
-  currentQuestion.answers.choices.forEach((choice, index) => {
-    const choiceButton = document.createElement('button');
-    choiceButton.textContent = choice;
-// Add event listener 
-    choicesContainer.appendChild(choiceButton);
-    });
-}
+    {
+      question: "What is an object in JavaScript?",
+      answers: ["A collection of properties", "An ordered list", "An image"],
+      correct: "A collection of properties"
+    },
+  
+    {
+      question: "How can you declare a variable in JavaScript?",
+      answers: ["var x", "let x", "const x"],
+      correct: "var x"
+    },
+  
+    {
+      question: "Which property is used to set the background color in CSS?",
+      answers: ["color", "background-color", "bgcolor"],
+      correct: "background-color"
+    },
+  
+    {
+      question: "How do you comment out a line in JavaScript?",
+      answers: ["/* comment */", "// comment", "<!-- comment -->"],
+      correct: "// comment"
+    }
+  ];
+  
+  let currentQuestionIndex = 0;
